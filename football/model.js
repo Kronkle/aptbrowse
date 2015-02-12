9495a7ffbc3ed072424a44f194e39abd1480b511
Michael Kronk <mkronk7@gmail.com>
2015-02-11 23:58:52 -0500

    started view.js

---
+++ football/model.js
@@ -0,0 +1,27 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+
+ var addTableCell = {
+
+ 	initializeValue: function(value) {
+ 		this.set({ "value": value });
+ 	}
+
+ 	initializeHeading: function(value) {
+ 		this.set({ "value": value });
+ 	}
+
+ };
+
+ var displayTableCell = {
+
+ 	initializeValue: function(value) {
+ 		this.set({ "value": value});
+ 	}
+
+ };
+
