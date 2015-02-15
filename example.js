38a5f84dcb41907900f5ac82f736291497aaeae8
Michael Kronk <mkronk7@gmail.com>
2015-02-15 13:29:30 -0500

    dynamic table rows (filler) for mvc page

--- example.js
+++ example.js
@@ -3,4 +3,19 @@ function addRow() {
 	controller.loadView();
 };
 
-addRow();
+
+//Create "Add Apartment" button to submit a new player entry
+var submit=document.createElement("input");
+submit.type="button";
+				
+submit.value="Add Apartment";
+submit.className="center-block";
+
+submit.onclick=function(){
+	addRow();
+};
+
+var ftablebody = document.getElementById("intro");
+ftablebody.appendChild(submit);
+
+//Will define an event listener here for clicking the Add Apartment button, which will call addRow
--- view.js
+++ view.js
@@ -11,8 +11,25 @@ var displayRowView = function ( model ) {
 
 displayRowView.prototype.render = function () {
 
-	var html = '<tr><td>Data1</td><td>Data2</td><td>Data3</td></tr>';
-	document.getElementById('ftablebody').innerHTML = html;
+	var htmlRow = document.createElement("tr");
+
+	//Will use a handlebars template here in the future
+	//var html = '<tr><td>Data1</td><td>Data2</td><td>Data3</td></tr>';
+	for(var i = 0; i < 3; i++){
+		//Create filler text for each row field
+		var text = "Data" + i.toString();
+		var textNode = document.createTextNode(text);
+
+		//Append filler text to each row field
+		var td = document.createElement("td");
+		td.appendChild(textNode);
+
+		//Append row field to table row to be added
+		htmlRow.appendChild(td);
+	}
+
+	var ftablebody = document.getElementById("ftablebody");
+	ftablebody.appendChild(htmlRow);
 
 };
 
