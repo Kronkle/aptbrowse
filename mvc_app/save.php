fff1087893a64ba97557561376438fd54bf94d6d
Michael Kronk <mkronk7@gmail.com>
2015-03-18 21:41:33 -0400

    output table rows are saved

--- mvc_app/save.php
+++ mvc_app/save.php
@@ -1,10 +1,12 @@
 <?php
 
-#Include the FirePHP class
+//Include the FirePHP class
 require_once('FirePHPCore/FirePHP.class.php');
-#Start buffering the output. Not required if output_buffering is set on in php.ini file
+
+//Start buffering the output. Not required if output_buffering is set on in php.ini file
 ob_start();
-#get a firePHP variable reference
+
+//get a firePHP variable reference
 $firephp = FirePHP::getInstance(true);
 
 // declare variables from form and set to empty strings
@@ -16,14 +18,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$pass   = $_POST["pass"];
 }
 
+$firephp->log("FirePHP Test Output!");
 
-$firephp->log("Output is this!!!");
 //MySQL processing will go here
 $servername = "localhost";
 $username = "root";
 $password = "password";
 $db = "aptbrowseDB";
 
+//Connect to the aptbrowse db
 $connect = new mysqli($servername, $username, $password, $db);
 
 if ($connect->connect_error) {
@@ -46,6 +49,31 @@ if ($connect->query($sql) === TRUE) {
 	echo "Error creating table $pass: " . $connect->error;
 }
 
+//Iterate through html in output and pass into newly created table
+$dom = new DOMDocument;
+$dom->loadHTML($output);
+
+//Clean out top-level dom elements
+$dom->removeChild($dom->doctype);
+$dom->replaceChild($dom->firstChild->firstChild->firstChild, $dom->firstChild);
+
+//Declare variables to hold html elements in output table
+$tableTags = $tableBodyTags = $tableRowTags = "";
+
+//Sort each tag type into a different variable
+$tableTags = $dom->getElementsByTagName("table");
+
+//Extras for debugging
+$tableBodyTags = $dom->getElementsByTagName('tbody');
+$tableRowTags = $dom->getElementsByTagName('tr');
+
+$tableInputElements = array();
+
+foreach($dom->getElementsByTagName("tr") as $node) {
+		$tableInputElements[] = $dom->saveHTML($node);
+}
+
+print_r($tableInputElements);
 $connect->close();
 
 /*Push current table output to a unique table in the database, generate a random
