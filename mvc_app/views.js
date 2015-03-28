603645bbf727b2319c4bba26e7ab1396f5593527
Kronkle <mkronk7@gmail.com>
2015-03-28 14:10:46 -0400

    Fixed U.S. zipcode validation, added back in map div for google places requests

--- mvc_app/views.js
+++ mvc_app/views.js
@@ -101,12 +101,13 @@ buttonPanelView.prototype.render = function () {
 
 		if (zip) {
 			//U.S. zipcode validation - 5 digits or 5 digits followed by hyphen and 4 digits
-			var regex = new RegExp("/(^\d{5}-\d{4}$)|(^\d{5}$)/")
+			var regex = new RegExp(/(^\d{5}-\d{4}$)|(^\d{5}$)/)
 			if (regex.test(zip)){
-				alert("Please enter a valid zipcode.");
+				addDisplayRowsThroughSearch(zip);
+			} else {
+				alert("Please enter a valid U.S. zipcode.");
 				return;
 			}
-			addDisplayRowsThroughSearch(zip);
 		}
 	};
 
