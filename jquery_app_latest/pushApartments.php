db2615538015c5d56efe7e0a6785c5e2ba4c56fb
Kronkle <mkronk7@gmail.com>
2015-04-26 12:14:38 -0400

    cleanup in pushApartments.php

--- jquery_app_latest/pushApartments.php
+++ jquery_app_latest/pushApartments.php
@@ -1,17 +1,17 @@
 <?php
 
-//Include the FirePHP class for debugging
+// Include the FirePHP class for debugging
 require_once('FirePHPCore/FirePHP.class.php');
 
-//Start buffering the output. Not required if output_buffering is set on in php.ini file
+// Start buffering the output. Not required if output_buffering is set on in php.ini file
 ob_start();
 
-//get a firePHP variable reference
+// Get a firePHP variable reference
 $firephp = FirePHP::getInstance(true);
 
 $firephp->log("FirePHP Test Output!");
 
-// declare variables from form and set to empty strings
+// Set HTML for heading row of search results table
 $response = 
 		'<tr id="topOutputRow">
 		<td style="overflow: hidden; white-space: nowrap;">Name</td>
@@ -26,6 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$str_json = json_decode(file_get_contents("php://input"));
 }
 
+// Formulate HTML for content rows of search results table
 foreach($str_json as $apartment) {
 	$response .= 
 			'<tr>
@@ -48,28 +49,6 @@ foreach($str_json as $apartment) {
 }
 
 echo $response;
-/*
-if ($url == "Website not listed in Google Maps"){
-	// avoid outputting html with a blank url when Google doesn't find an apartment website
-	echo '<tr>
-	<td style="overflow: hidden; white-space: nowrap;">'. $name.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $address.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $rent.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $amenities.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $pets.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $url.
-	'</td></tr>';
-} else {
-	// return with html for a new apartment row when an apartment website is found
-	echo '<tr>
-	<td style="overflow: hidden; white-space: nowrap;">'. $name.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $address.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $rent.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $amenities.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. $pets.
-	'</td><td style="overflow: hidden; white-space: nowrap;">'. '<a href="'.$url.'" target="_blank">Website</a>'.
-	'</td></tr>';
-}*/
 
 ?>
 
