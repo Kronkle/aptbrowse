87cf995aecfe15799f80ccb0c7cf4236a4f60f52
unknown <mkronk7@gmail.com>
2015-05-01 16:39:36 -0400

    styling for Sign Up/Sign In dropdowns

--- jquery_app_latest/register.php
+++ jquery_app_latest/register.php
@@ -1,16 +1,16 @@
 <?php
 
 if ( !empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']) ) {
-	// user is logged in, allow access to app
-	//Return HTML for account name at the top right of navbar
+	// user is logged in, allow access to saved search results
+	// Also return HTML for account name at the top right of navbar
 }
 elseif ( !empty($POST['username']) && !empty($_POST['password']) ) {
-	// user is logging in
-	//Eliminate login/registration popover
+	// user is signing up, check db for username to see if it already exists
+	// if not, place username and password in accounts db
+
 }
 else {
-	// user isn't logged in, display login/registration form
-	//Echo bootstrap popover here
+	// user isn't logged in allow access to basic app only
 }
 
 
