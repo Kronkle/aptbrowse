39326b4bf17c39938afe6a4281ae31aef91eb6d2
unknown <mkronk7@gmail.com>
2015-05-11 16:40:14 -0400

    consistent style in views.js, updated comments, striking comments, vanilla JS -> jQuery where appropriate

--- jquery_app_latest/views.js
+++ jquery_app_latest/views.js
@@ -1,81 +1,92 @@
 /* 
-*  Two views will be presented for now: a view for rendering the search bar and a view
-*  for displaying the search results.
-*/
+ *  zipSearchView     - Triggers AJAX calls and controller events when user interacts with the navbar and searchbar
+ *  apartmentListView - Renders apartment search results
+ */
 
 var zipSearchView = function ( zipSearchModel, zipSearchController, resultsModel, resultsController ) {
-	$( document ).ready(function() {
+	$( document ).ready( function () {
 		var apartmentListModel = resultsModel;
 
-		//For initializing the accounts and results dbs in admin mode (move this to a different view?)
-		$( "#initDB" ).on("click", function ( event ) {
+
+		// ***************************************************************************************
+		// ---------------------------------------NAVBAR------------------------------------------
+		// ***************************************************************************************
+
+
+		// For creating the MySQL "accounts" and "results" dbs in ADMIN mode - for testing
+		$( "#initDB" ).on( "click", function ( event ) {
 			$.ajax({
 				url: "initDB.php",
 				data: {},
 				success: function ( data ) {
-					alert("User and results databases have been created");
+					alert( "Accounts and results databases have been created" );
 				}
 			});
 		});
 
-		//For deleting the accounts and results dbs in admin mode (move this to a different view?)
-		$( "#clearDB" ).on("click", function ( event ) {
+		// For deleting the MySQL "accounts" and "results" dbs in ADMIN mode - for testing
+		$( "#clearDB" ).on( "click", function ( event ) {
 			$.ajax({
 				url: "clearDB.php",
 				data: {},
 				success: function ( data ) {
-					alert("User and results databases have been deleted");
+					alert( "Accounts and results databases have been deleted" );
 				}
 			});
 		});
 
-		//Create "Zip Code Search" button for auto-filling table with searches via Google Maps
-		$(".content").on('click', '.zipSearchBtn', function () {
-			zipSearchController.handleEvent( apartmentListModel, resultsController );
-		});
+		// For registering a new user when "Register" button is clicked
+		$( "#registerBtn" ).on( "click", function () {
 
-		var register=document.getElementById("registerBtn");
-		register.addEventListener( "click", function ( ) {
-			var username = document.getElementById("usernameR").value;
-			
-			//Password validation here
-			var password = document.getElementById("passwordR").value;
-			var confirmPassword = document.getElementById("passwordR2").value;
+			var username = $( "#usernameR" ).val();
+			var password = $( "#passwordR" ).val();
+			var confirmPassword = $( "#passwordR2" ).val();
+
+			// Validate that all fields are filled in properly
 
 			if ( username == "" || password == "" || confirmPassword == "" ) {
-				alert("Please fill out all fields");
+				alert( "Please fill out all fields" );
 			}
 
 			if ( password == confirmPassword ) {
 				zipSearchController.registerUser( apartmentListModel, resultsController, username, password );
 		    } else {
-	        	alert("Two different passwords entered");   
+	        	alert( "Two different passwords entered" );   
 		    }
 		});
 
-		var login=document.getElementById("loginBtn");
-		login.addEventListener( "click", function ( ) {
-			var username = document.getElementById("usernameL").value;
-			//Password validation here
-			var password = document.getElementById("passwordL").value;
+		// For logging in an existing user when "Sign In" button is clicked
+		$( "#loginBtn" ).on( "click", function() {
 
+			var username = $("#usernameL").val();
+			var password = $("#passwordL").val();
+
+			// Validate that all fields are filled in properly
 			if ( username == "" || password == "" ) {
-				alert("Please fill out all fields");
+				alert( "Please fill out all fields" );
 			} else {
 				zipSearchController.loginUser( apartmentListModel, resultsController, username, password );
 			}
 		});
 
-		//For resetting homepage when AptBrowse logo is clicked - clone both div and attached event handlers
-		//var homeClone = $(".searchContent").clone(true, true);
-
-		//Either change zipSearchView into a generic homepage view or add another view later for registration/login specifically
-		$(".navigationMenu").on('click', '#home', function () {
-			$(".search").show();
-			$(".break").show();
-			$(".ftablebody").html("");
+		// For refreshing the homepage when "AptBrowse" is clicked
+		$( ".navigationMenu" ).on( "click", "#home", function () {
+			$( ".search" ).show();
+			$( ".break" ).show();
+			$( ".ftablebody" ).html( "" );
 		});
-	});
+
+
+		// ***************************************************************************************
+		// ---------------------------------------SEARCHBAR---------------------------------------
+		// ***************************************************************************************
+
+
+		// For initial processing of searchbar input
+		$( ".content" ).on( "click", ".zipSearchBtn", function () {
+			zipSearchController.handleEvent( apartmentListModel, resultsController );
+		});	
+	});		
 	return this;
 };
 
@@ -84,13 +95,13 @@ var apartmentListView = function () {
 };
 
 apartmentListView.prototype.render = function ( data ) {
-	$(document).ready(function(){
+	$( document ).ready( function() {
 
-		// Remove spinner and the break element below
-		$(".spinner").hide();
-		$(".break").hide();
+		// Hide spinner and the break element below it
+		$( ".spinner" ).hide();
+		$( ".break" ).hide();
 
-		// Populate search results table
-		$(".ftablebody").html(data);
+		// Populate search results table with apartment data
+		$( ".ftablebody" ).html( data );
 	});
 };
