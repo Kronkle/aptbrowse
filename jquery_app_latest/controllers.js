0f5aa7a73bebce2026da644dcdcefa636a479552
unknown <mkronk7@gmail.com>
2015-05-12 16:35:46 -0400

    consistent formatting, vanilla JS to jQuery in controllers.js

--- jquery_app_latest/controllers.js
+++ jquery_app_latest/controllers.js
@@ -1,15 +1,55 @@
 /*
- * zipSearchController: Use user input to search
- * for apartments via the Google Places API,
- * update the model with results
- *
- * apartmentListController: Use apartment
- * information passed from the model to update
- * the view
-*/
+ *  zipSearchController     - Query the Google Places API with user search input and update apartmentListModel with results
+ *  apartmentListController - Use search results HTML generated from the model to update the apartmentListView
+ */
 
 var zipSearchController = function () {
 	return this;
+};
+
+zipSearchController.prototype.registerUser = function ( resultsModel, resultsController, username, password ) {
+
+    // For use with greeting upon registering
+    var username = username;
+
+    var data = "username=" + username + "&" + "password=" + password;
+
+    $.ajax({
+    	url: "register.php",
+    	data: { data };
+    	success: function ( data ) {
+    		if ( data ) {
+    			alert( "Username is already taken" );
+    		} else {
+    			alert( "Account created!" );
+    			$( "#registerMenu" ).hide();
+    			$( "#loginMenu" ).html( "<li>Welcome, " + username + "</li>" );
+    		}
+    	},
+    	error: function () {
+    		alert( "Problem with registration request" );	
+    	}
+    });
+};
+
+zipSearchController.prototype.loginUser = function ( resultsModel, resultsController, username, password ) {
+
+    // For use with greeting upon login
+    var username = username;
+    
+    var data = "username=" + username + "&" + "password=" + password;
+
+    $.ajax({
+    	url: "login.php",
+    	data: { data };
+    	success: function ( data ) {
+ 			$( "#registerMenu" ).hide();
+ 			$( "#loginMenu" ).html( "<li>Welcome, " + username + "</li>" );
+    	},
+    	error: function () {
+    		alert( "Problem with login request" );
+    	}
+    });
 };
 
 zipSearchController.prototype.handleEvent = function ( resultsModel, resultsController ) {
@@ -19,141 +59,75 @@ zipSearchController.prototype.handleEvent = function ( resultsModel, resultsCont
 		var zip = $( ".zipSearch" ).val();
 
 		if ( zip ) {
-			//U.S. zipcode validation - 5 digits or 5 digits followed by hyphen and 4 digits
-			var regex = new RegExp(/(^\d{5}-\d{4}$)|(^\d{5}$)/)
-			if (regex.test(zip)){							
-				this.zipSearch(zip, resultsModel, resultsController);
-				
-				// Hide searchbar and add loading spinner in place of the searchbar
-				$(".search").hide();
-				$(".spinner").show();
 
+			// U.S. zipcode validation - 5 digits or 5 digits followed by hyphen and 4 digits
+			var regex = new RegExp(/(^\d{5}-\d{4}$)|(^\d{5}$)/)
+
+			if ( regex.test( zip ) ){							
+				this.zipSearch( zip, resultsModel, resultsController );
+				
+				// Hide searchbar and show loading spinner
+				$( ".search" ).hide();
+				$( ".spinner" ).show();
 			} else {
-				alert("Please enter a valid U.S. zipcode.");
-				return;
+				alert( "Please enter a valid U.S. zipcode." );
 			}
 		}
 };
 
-zipSearchController.prototype.registerUser = function ( resultsModel, resultsController, username, password ) {
-	//Set up AJAX request with inputted data
-    var request = new XMLHttpRequest();
+zipSearchController.prototype.zipSearch = function ( zip, resultsModel, resultsController ) {
 
-    //For use with greeting upon registering
-    var username = username;
-
-    var data = "username=" + username + "&" + "password=" + password;
-
-	//Open the request, set the header, and send the data
-	request.open('POST', 'register.php', true);
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send(data);
-		
-	//After the data has been sanitized, display on the HTML page within ftable
-	request.onreadystatechange = display_data;
-	function display_data() {
-		if (request.readyState == 4) {
-			if(request.status == 200) {
-				var data=request.responseText;
-				if ( data ){
-					alert("Username is already taken");
-				} else {
-					alert("Account created!");
-					$( "#registerMenu" ).hide();
-					$( "#loginMenu" ).html("<li>Welcome, " + username + "</li>");
-					
-				}
-		 	} else {
-				alert('Problem with registration request');	
-		 	}
-		}		
-	};
-};
-
-zipSearchController.prototype.loginUser = function ( resultsModel, resultsController, username, password ) {
-
-	//Set up AJAX request with inputted data
-    var request = new XMLHttpRequest();
-
-    //For use with greeting upon login
-    var username = username;
-    
-    var data = "username=" + username + "&" + "password=" + password;
-
-	//Open the request, set the header, and send the data
-	request.open('POST', 'login.php', true);
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send(data);
-		
-	//After the data has been sanitized, display on the HTML page within ftable
-	request.onreadystatechange = display_data;
-	function display_data() {
-		if (request.readyState == 4) {
-			if(request.status == 200) {
-				var data=request.responseText;
-				$( "#registerMenu" ).hide();
-				$( "#loginMenu" ).html("<li>Welcome, " + username + "</li>");
-				console.log(data);				
-		 	} else {
-				alert('Problem with login request');	
-		 	}
-		}		
-	};
-};
-
-//Use this function to submit a Google places search for apartments in the zipcode
-zipSearchController.prototype.zipSearch = function (zip, resultsModel, resultsController) {
-
-	//Convert user zipcode into lat/lng coordinates for google places text search
+	// Create Geocoder and relevant variables for zipcode to lat/lng coordinates conversion
 	var geocoder = new google.maps.Geocoder();
 	var address = zip;
 	var lat = '';
 	var lng = '';
+
 	var me = this;
 	var apartmentListModel = resultsModel;
 
 	// Include callback function since geocoder method works asynchronously
-	geocoder.geocode( {'address': address}, function(results_array, status) { 
-		if (status == google.maps.GeocoderStatus.OK){
+	geocoder.geocode( { 'address': address }, function( results_array, status ) { 
+		if ( status == google.maps.GeocoderStatus.OK ){
 	        
+	        // Convert user zipcode into lat/lng coordinates for Google Places text search
 	        lat = results_array[0].geometry.location.lat()
         	lng = results_array[0].geometry.location.lng()
         	var loc = new google.maps.LatLng(lat, lng);
 
+        	// Form Google Places request with location specified by lat/lng coordinates, a radius of 5 miles, and "Apartment" search query
 			var request = {
 				location: loc,
 				radius: '5',
 				query: "Apartment",
 			};
 
-			var service = new google.maps.places.PlacesService(document.getElementById('map'));
-			service.textSearch(request, function (results, status) {
-				if (status == google.maps.places.PlacesServiceStatus.OK){
-					console.log(results);
-					//Iterate through each apartment result, get details, and update apartmentList model
-					for(var i = 0; i < results.length; i++){
-						//Start passing i here to a function that can notify when results are completely recorded
+			var service = new google.maps.places.PlacesService( document.getElementById( "map" ) );
+
+			service.textSearch( request, function ( results, status ) {
+				if ( status == google.maps.places.PlacesServiceStatus.OK ){
+
+					// Iterate through each search result and get details for each apartment listing
+					for( var i = 0; i < results.length; i++ ){
+
+						// Set done to 1 when details have been gathered for each apartment listing
 						var done;
-						if (i == (results.length -1)){
+
+						if ( i == ( results.length -1 ) ){
 							done = 1;
-						}
-						else {
+						} else {
 							done = 0;
 						}
-						me.getApartmentDetails(results[i], service, done, apartmentListModel, resultsController);				
+						me.getApartmentDetails( results[i], service, done, apartmentListModel, resultsController );				
 					}
-					//If query is successful and results are available, update ApartmentList Model below:
 				}
 			});
-		    	}
-		else {
-		    alert("Google Maps text search wasn't successful. Please try again later.");
-		    console.log(status);
+		} else {
+		    alert( "Google Maps text search wasn't successful. Please try again later." );
 		}
 	});	
 };
 
-//Get details for specific apartment object
 zipSearchController.prototype.getApartmentDetails = function ( results, service, done, resultsModel, resultsController ) {
 	var me = this; 
 	var done = done;
@@ -163,7 +137,7 @@ zipSearchController.prototype.getApartmentDetails = function ( results, service,
 		placeId: results.place_id
 	};
 	
-	//Final object to be passed to the apartmentList model
+	// Final object to be passed to the apartmentListModel which represents all details of a particular apartment listing
 	var aptObject = {
 				    	name: "Name",
 				    	address: "Address",
@@ -179,34 +153,30 @@ zipSearchController.prototype.getApartmentDetails = function ( results, service,
 	getDetails();
 
 	function getDetails() {
-		service.getDetails(request, function (results, status) {
-			if (status == google.maps.places.PlacesServiceStatus.OK){
-				console.log(results);
+		service.getDetails( request, function ( results, status ) {
+			if ( status == google.maps.places.PlacesServiceStatus.OK ){
 				aptObject.url = results.website;
-				me.updateApartmentListModel(aptObject, done, apartmentListModel, resultsController);
+				me.updateApartmentListModel( aptObject, done, apartmentListModel, resultsController );
 			}
 			else {
-				//Setting timeout to avoid hitting query limit
-				setTimeout(function () {
+				// Setting timeout to avoid hitting Google Places query limit
+				setTimeout( function () {
 					getDetails();
-				}, 200);			
+				}, 200 );			
 			}		
 		});
 	};
 };
 
-//Add apartment with details to apartment list model
 zipSearchController.prototype.updateApartmentListModel = function ( apartment, done, resultsModel, resultsController ) {	
-	resultsModel.addApartment(apartment.name, apartment.address, apartment.rating, apartment.hours, apartment.phone, apartment.url, done, resultsController);
+	resultsModel.addApartment( apartment.name, apartment.address, apartment.rating, apartment.hours, apartment.phone, apartment.url, done, resultsController );
 };
 
-//Accept a resultsView for displaying search results
 var apartmentListController = function ( resultsView ) {
 	this.resultsView = resultsView;
 	return this;
 };
 
-//Render search results via resultsView 
 apartmentListController.prototype.updateApartmentListView = function ( data ) {
 	this.resultsView.render( data );
 };
