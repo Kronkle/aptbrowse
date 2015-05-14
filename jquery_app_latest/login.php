aa6aa0ef2a09784317fc671c09e9b6ddc58df3ff
unknown <mkronk7@gmail.com>
2015-05-14 14:45:15 -0400

    consistent formatting, striking comments, refactoring in login.php and register.php

--- jquery_app_latest/login.php
+++ jquery_app_latest/login.php
@@ -1,40 +1,56 @@
 <?php
-// Handle user login here
-session_start();
-// Include the FirePHP class for debugging
-require_once('FirePHPCore/FirePHP.class.php');
+/* 
+ *  login.php Functionality:
+ *  1) Start FirePHP logging for testing and debugging
+ *  2) Validate username/password input
+ */
 
-// Start buffering the output. Not required if output_buffering is set on in php.ini file
+// Begin a session for persistent user login
+session_start();
+
+// ***************************************************************************************
+// ---------------------------------------FirePHP-----------------------------------------
+// ***************************************************************************************
+
+require_once( "FirePHPCore/FirePHP.class.php" );
+
+// Start buffering the output - not required if output_buffering is set on in php.ini file
 ob_start();
+
+// Get a firePHP variable reference
+$firephp = FirePHP::getInstance( true );
+
+$firephp->log( "FirePHP Test Output!" );
+
+// ***************************************************************************************
+// ---------------------------------Validate username/password----------------------------
+// ***************************************************************************************
 
 $servername = "localhost";
 $dbname = "accounts";
 $username = "root";
 $password = "password";
 
-$connect = new mysqli($servername, $username, $password);
-$connect->select_db($dbname);
+$connect = new mysqli( $servername, $username, $password );
+$connect->select_db( $dbname );
 
-// Get a firePHP variable reference
-$firephp = FirePHP::getInstance(true);
+if ( !empty( $_POST[ "username" ] ) && !empty( $_POST[ "password" ] ) ) {
 
-if (!empty($_POST['username']) && !empty($_POST['password'])) {
+	$username = $connect->real_escape_string( $_POST[ "username" ] );
+	$password = md5($connect->real_escape_string( $_POST[ "password" ] ) );
 
-	$username = $connect->real_escape_string($_POST['username']);
-	$password = md5($connect->real_escape_string($_POST['password']));
-
-	// Check accounts db for inputted username in users table
-	$sql = $connect->query("SELECT * FROM users WHERE Username = '".$username."' AND Password = '".$password."'");
+	// Check "accounts" database for inputted username in users table
+	$sql = $connect->query( "SELECT * FROM users WHERE Username = '".$username."' AND Password = '".$password."'" );
 
 	if ( $sql->num_rows == 1 ) {
-		$firephp->log("You've logged in, $username");
+		$firephp->log( "You've logged in, $username" );
 	} else {
-			$firephp->log("Login credentials failed");
-			http_response_code(400);
+			$firephp->log( "Login credentials failed" );
+			http_response_code( 400 );
 	}
 } else {
-	$firephp->log("Error receiving login username/password from browser");
-	http_response_code(400);
+	$firephp->log( "Error receiving login username/password from browser" );
+	http_response_code( 400 );
 }
 
 
--- jquery_app_latest/register.php
+++ jquery_app_latest/register.php
@@ -1,46 +1,62 @@
 <?php
-// Handle user registration here
-session_start();
-// Include the FirePHP class for debugging
-require_once('FirePHPCore/FirePHP.class.php');
+/* 
+ *  register.php Functionality:
+ *  1) Start FirePHP logging for testing and debugging
+ *  2) Validate username/password input
+ */
 
-// Start buffering the output. Not required if output_buffering is set on in php.ini file
+// Begin a session for persistent user login
+session_start();
+
+// ***************************************************************************************
+// ---------------------------------------FirePHP-----------------------------------------
+// ***************************************************************************************
+
+require_once( "FirePHPCore/FirePHP.class.php" );
+
+// Start buffering the output - not required if output_buffering is set on in php.ini file
 ob_start();
+
+// Get a firePHP variable reference
+$firephp = FirePHP::getInstance( true );
+
+$firephp->log( "FirePHP Test Output!" );
+
+// ***************************************************************************************
+// ---------------------------------Validate username/password----------------------------
+// ***************************************************************************************
 
 $servername = "localhost";
 $dbname = "accounts";
 $username = "root";
 $password = "password";
 
-$connect = new mysqli($servername, $username, $password);
-$connect->select_db($dbname);
+$connect = new mysqli( $servername, $username, $password );
+$connect->select_db( $dbname );
 
-// Get a firePHP variable reference
-$firephp = FirePHP::getInstance(true);
+if ( !empty( $_POST[ "username" ] ) && !empty( $_POST[ "password" ] ) ) {
 
-if (!empty($_POST['username']) && !empty($_POST['password'])) {
+	$username = $connect->real_escape_string( $_POST[ "username" ] );
+	$password = md5( $connect->real_escape_string( $_POST[ "password" ] ) );
 
-	$username = $connect->real_escape_string($_POST['username']);
-	$password = md5($connect->real_escape_string($_POST['password']));
-
-	// Check accounts db for inputted username in users table
-	$sql = $connect->query("SELECT * FROM users WHERE Username = '".$username."'");
+	// Check "accounts" database for inputted username in users table
+	$sql = $connect->query( "SELECT * FROM users WHERE Username = '".$username."'" );
 
 	if ( $sql->num_rows == 1 ) {
 		echo "Username is already taken";
-		http_response_code(400);
+		http_response_code( 400 );
 	} else {
-		$sql = $connect->query("INSERT INTO users (Username, Password) VALUES('".$username."', '".$password."')");
+		$sql = $connect->query( "INSERT INTO users (Username, Password) VALUES('".$username."', '".$password."')" );
 		if ( $sql ) {
-			$firephp->log("Account created");
+			$firephp->log( "Account created" );
 		} else {
-			$firephp->log("Error creating account");
-			http_response_code(400);
+			$firephp->log( "Error creating account" );
+			http_response_code( 400 );
 		}
 	}
 } else {
-	$firephp->log("Error receiving username/password from browser");
-	http_response_code(400);
+	$firephp->log( "Error receiving username/password from browser" );
+	http_response_code( 400 );
 }
 
 
