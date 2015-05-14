3f75ed46eceeedc1dc09e8ae46468b75bacb5f3d
unknown <mkronk7@gmail.com>
2015-05-14 14:58:03 -0400

    refactoring in starter.js

--- jquery_app_latest/starter.js
+++ jquery_app_latest/starter.js
@@ -5,14 +5,22 @@
 
 function init() {	
 
+	// Processes user zipcode searches entered into zipSearchView and updates apartmentListModel
 	var searchController = new zipSearchController;
-	var searchModel = new zipSearchModel;
 
+	// Models additional information about zipcode searches
+	var searchModel = new zipSearchModel;
+	
+	// Models a list of apartment results from user zipcode searches
 	var resultsModel = new apartmentListModel;
 
+	// Renders list of apartments from apartmentListModel
 	var resultsView = new apartmentListView;
+
+	// Notifies apartmentListView of changes to the aparmentListModel
 	var resultsController = new apartmentListController( resultsView );	
 
+	// Renders zipcode searchbar
 	var searchView = new zipSearchView( searchModel, searchController, resultsModel, resultsController );
 };
 
