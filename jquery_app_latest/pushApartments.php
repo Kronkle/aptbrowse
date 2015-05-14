2d64bcc08e69912f55cdaf9741415365b18ed596
unknown <mkronk7@gmail.com>
2015-05-14 13:50:40 -0400

    consistent formatting, striking comments, refactoring in pushApartments.php

--- jquery_app_latest/pushApartments.php
+++ jquery_app_latest/pushApartments.php
@@ -1,17 +1,29 @@
 <?php
+/* 
+ *  pushApartments.php Functionality:
+ *  1) Start FirePHP logging for testing and debugging
+ *  2) Create HTML for the heading of the search results table
+ *  3) Use Google Places results to create HTML for search results table content
+ */
 
-// Include the FirePHP class for debugging
-require_once('FirePHPCore/FirePHP.class.php');
+// ***************************************************************************************
+// ---------------------------------------FirePHP-----------------------------------------
+// ***************************************************************************************
 
-// Start buffering the output. Not required if output_buffering is set on in php.ini file
+require_once( "FirePHPCore/FirePHP.class.php" );
+
+// Start buffering the output - not required if output_buffering is set on in php.ini file
 ob_start();
 
 // Get a firePHP variable reference
-$firephp = FirePHP::getInstance(true);
+$firephp = FirePHP::getInstance( true );
 
-$firephp->log("FirePHP Test Output!");
+$firephp->log( "FirePHP Test Output!" );
 
-// Set HTML for heading row of search results table
+// ***************************************************************************************
+// ---------------------------------Create heading row HTML-------------------------------
+// ***************************************************************************************
+
 $response = 
 		'<tr id="topOutputRow">
 		<td style="overflow: hidden; white-space: nowrap;">Name</td>
@@ -22,12 +34,17 @@ $response =
 		<td style="overflow: hidden; white-space: nowrap;">Website</td>
 		</tr>';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$str_json = json_decode(file_get_contents("php://input"));
+// ***************************************************************************************
+// ---------------------------------Create table content HTML-----------------------------
+// ***************************************************************************************
+
+if ( $_SERVER[ "REQUEST_METHOD" ] == "POST" ) {
+	$str_json = json_decode( file_get_contents( "php://input" ) );
 }
 
-// Formulate HTML for content rows of search results table
-foreach($str_json as $apartment) {
+// Generate a table row for each apartment in search results
+foreach( $str_json as $apartment ) {
+
 	$response .= 
 			'<tr>
 			<td style="overflow: hidden; white-space: nowrap;">'. $apartment->Name.
@@ -35,15 +52,17 @@ foreach($str_json as $apartment) {
 			'</td><td style="overflow: hidden; white-space: nowrap;">'. $apartment->Rating.
 			'</td><td style="overflow: hidden; white-space: nowrap;">'. $apartment->Hours.
 			'</td><td style="overflow: hidden; white-space: nowrap;">'. $apartment->Phone;
-	if(!(isset($apartment->URL))){
+
+	// Use filler text for URL field if website isn't listed in Google Maps
+	if( !( isset( $apartment->URL ) ) ){
 		$apartment->URL = "Website not listed in Google Maps";
 		$response .=
-		'</td><td style="overflow: hidden; white-space: nowrap;">'. $apartment->URL;
-	}
-	else {
+			'</td><td style="overflow: hidden; white-space: nowrap;">'. $apartment->URL;
+	} else {
 		$response .= 
-		'</td><td style="overflow: hidden; white-space: nowrap;">'. '<a href="'.$apartment->URL.'" target="_blank">Website</a>';
+			'</td><td style="overflow: hidden; white-space: nowrap;">'. '<a href="'.$apartment->URL.'" target="_blank">Website</a>';
 	}
+
 	$response .=
 			'</td></tr>';
 }
